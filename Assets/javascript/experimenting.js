b0d03c62436ad542ed5600563556e29d6e76bc94
Aaron Yacher <29718914+aalink@users.noreply.github.com>
2022-03-19 21:21:13 -0400

    New string functions and object properties usersChoices split into 2 functions to ensure all options are included

--- Assets/javascript/experimenting.js
+++ Assets/javascript/experimenting.js
@@ -4,6 +4,8 @@ const everyChar = {
   lowercase: "abcdefghijklmnopqrstuvwxyz",
   numbers: "0123456789",
   special: "!@#$%^&*()_+=-[]{}|/?><`~",
+  primaryString: "",
+  secondaryString: "",
   password: "",
 };
 
@@ -16,58 +18,101 @@ function ranNumbers(range) {
 //✅ Function that will randomize any string
 function randomizeAnyString(stringToRandomize) {
   stringDone = "";
-  randomizeThese = stringToRandomize
+  randomizeThese = stringToRandomize;
   for (let step = 0; step < randomizeThese.length; step++)
     stringDone += randomizeThese.charAt(ranNumbers(randomizeThese.length));
 
-    // console.log("This is stringDone: " + stringDone)
-    return stringDone; 
+  // console.log("This is stringDone: " + stringDone)
+  return stringDone;
+}
+
+//✅ Uses true/false boolean to record the user's choice of character types.
+//It only takes 1 of each chosen character type and stores it in everyChar.primaryString
+function userChoicesStringOne(upper, lower, numbers, special) {
+  if (upper === true) {
+    upperRandom = randomizeAnyString(everyChar.uppercase);
+    everyChar.primaryString += upperRandom.charAt(0);
+    // console.log(everyChar.primaryString)
+  }
+  if (lower === true) {
+    lowerRandom = randomizeAnyString(everyChar.lowercase);
+    everyChar.primaryString += lowerRandom.charAt(0);
+    // console.log(everyChar.primaryString)
+  }
+  if (numbers === true) {
+    numberRandom = randomizeAnyString(everyChar.numbers);
+    everyChar.primaryString += numberRandom.charAt(0);
+    // console.log(everyChar.primaryString)
+  }
+  if (special === true) {
+    specialRandom = randomizeAnyString(everyChar.special);
+    everyChar.primaryString += specialRandom.charAt(0);
+    // console.log(everyChar.primaryString)
+  }
+
+  return everyChar.primaryString;
+  // console.log(randomizeAnyString(everyChar.primaryString))
+}
+
+//✅Finds the number of characters in everyChar.primaryString and then subtracts it from passwordLengthChosen. The number that is returned will be used to generate everyChar.secondaryString
+function chosenPasswordLength(passwordLengthChosen) {
+  numberOfChars = passwordLengthChosen;
+  //  console.log("The user wants this many: " + numberOfChars)
+  numberAlreadyInPassword = everyChar.password.length;
+  //  console.log("The password already contains this many: " + numberAlreadyInPassword)
+  //  console.log(numberOfChars - numberAlreadyInPassword)
+  //  console.log("Primary string has this many characters: " + everyChar.primaryString.length)
+  //  console.log("Secondary string has this many characters: " + everyChar.secondaryString.length)
+  //  console.log("The password needs this many more characters: " + (numberOfChars - everyChar.primaryString.length))
+
+  return numberOfChars - everyChar.primaryString.length;
 }
 
 //✅ Uses true/false boolean to record the user's choice of character types.
 //It only takes 1 of each chosen character type and stores it in everyChar.password
-function usersChoices(upper, lower, numbers, special) {
+function userChoicesStringTwo(upper, lower, numbers, special) {
   if (upper === true) {
-    upperRandom = randomizeAnyString(everyChar.uppercase)
-    everyChar.password += upperRandom.charAt(0)
-    // console.log(everyChar.password)
+    upperRandom = randomizeAnyString(everyChar.uppercase);
+    everyChar.secondaryString += upperRandom;
+    // console.log(everyChar.secondaryString)
   }
   if (lower === true) {
-    lowerRandom = randomizeAnyString(everyChar.lowercase)
-    everyChar.password += lowerRandom.charAt(0)
-    // console.log(everyChar.password)
+    lowerRandom = randomizeAnyString(everyChar.lowercase);
+    everyChar.secondaryString += lowerRandom;
+    // console.log(everyChar.secondaryString)
   }
   if (numbers === true) {
-    numberRandom = randomizeAnyString(everyChar.numbers)
-    everyChar.password += numberRandom.charAt(0)
-    // console.log(everyChar.password)
+    numberRandom = randomizeAnyString(everyChar.numbers);
+    everyChar.secondaryString += numberRandom;
+    // console.log(everyChar.secondaryString)
   }
   if (special === true) {
-    specialRandom = randomizeAnyString(everyChar.special)
-    everyChar.password += specialRandom.charAt(0)
-    // console.log(everyChar.password)
+    specialRandom = randomizeAnyString(everyChar.special);
+    everyChar.secondaryString += specialRandom;
+    // console.log(everyChar.secondaryString)
   }
-  
-  return everyChar.password
-    // console.log(randomizeAnyString(everyChar.password))
-}
 
-x = usersChoices(true, true, true, true)
-console.log(x)
-//⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆
-//⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆
-//⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆
-
-function chosenPasswordLength(passwordLengthChosen) {
-   numberOfChars = passwordLengthChosen
-   console.log(numberOfChars)
-   numberAlreadyInPassword = everyChar.password.length
-   console.log(numberAlreadyInPassword)
-   console.log(numberOfChars - numberAlreadyInPassword)
+  return everyChar.secondaryString;
+  // console.log(randomizeAnyString(everyChar.secondaryString))
 }
 
 
+//⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆
+//⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆
+//⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆
+
+primary = userChoicesStringOne(true, true, true, true);
+secondary = userChoicesStringTwo(true, true, true, true);
+
+// console.log(userChoicesStringTwo(true,true,true,true))
+
+console.log(chosenPasswordLength(20));
+
+// console.log(primary);
+
+// console.log(secondary);
+
+console.log(primary + secondary);
 
 
-
-chosenPasswordLength(20)
+// chosenPasswordLength(20)
