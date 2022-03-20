8e9c1813bc13a03eb7a358f66884f8c18bb092bb
Aaron Yacher <29718914+aalink@users.noreply.github.com>
2022-03-20 10:52:02 -0400

    Removing unneeded function and 'for loop' fix. No need to have function: chosenPasswordLength() For loop fix that resolves issue with incorrect finalNumber calculation.

--- Assets/javascript/experimenting.js
+++ Assets/javascript/experimenting.js
@@ -82,40 +82,23 @@ function userChoicesStringTwo(upper, lower, numbers, special) {
   // console.log(randomizeAnyString(everyChar.secondaryString))
 }
 
-//✅Finds the number of characters in everyChar.primaryString and then subtracts it from passwordLengthChosen. The number that is returned will be used to generate everyChar.secondaryString
-function chosenPasswordLength(passwordLengthChosen) {
-  numberOfChars = passwordLengthChosen;
-  //  console.log("The user wants this many: " + numberOfChars)
-  numberAlreadyInPassword = everyChar.password.length;
-  //  console.log("The password already contains this many: " + numberAlreadyInPassword)
-  //  console.log(numberOfChars - numberAlreadyInPassword)
-  //  console.log("Primary string has this many characters: " + everyChar.primaryString.length)
-  //  console.log("Secondary string has this many characters: " + everyChar.secondaryString.length)
-  //  console.log("The password needs this many more characters: " + (numberOfChars - everyChar.primaryString.length))
-
-  return numberOfChars - everyChar.primaryString.length;
-}
 
 //⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆
 //⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆
 //⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆
 //✅Final function that takes 5 parameters: The number of characters in the password, and then true/false booleans the determine what types are permitted.
 function finalPassword(finalNumber,upperbool,lowerbool,numberbool,specialbool) {
+  
   everyChar.primaryString = userChoicesStringOne(upperbool, lowerbool, numberbool, specialbool);
-  console.log(everyChar.primaryString)
   
   everyChar.secondaryString = userChoicesStringTwo(upperbool, lowerbool, numberbool, specialbool);
-  console.log(everyChar.secondaryString)
 
   everyChar.password += everyChar.primaryString
   
-  subtracted = finalNumber - everyChar.primaryString.length;
-  console.log(subtracted)
-  for (let step = 0; step < subtracted; step++) {
-    everyChar.password += everyChar.secondaryString.charAt(ranNumbers(subtracted));
+  for (let step = 0; step < finalNumber - everyChar.primaryString.length; step++) {
+    everyChar.password += everyChar.secondaryString.charAt(ranNumbers(everyChar.secondaryString.length));
   }
   return everyChar.password;
 }
 
-console.log(finalPassword(10, true, true, false, false));
-console.log(everyChar)
+console.log(finalPassword(1000, true, true, true, true));
