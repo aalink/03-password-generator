09306eccb2306637a60a0422e8033c8da0e979be
Aaron Yacher <29718914+aalink@users.noreply.github.com>
2022-03-20 12:13:34 -0400

    Changes to runTheOtherPrompts() More conditional statements and logical operators are in this function. New message displays on webpage if all chosen character types are false.

--- script.js
+++ script.js
@@ -31,17 +31,21 @@ function generatePassword(randomString) {
   if (askPasswordLength !== false) {
     runTheOtherPrompts();
   }
-//⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️
-// Run this after the user chooses the length requirement of 8 to 128
+  // Run this after the user chooses the length requirement of 8 to 128
   function runTheOtherPrompts() {
-    storeAllCharsHere = ""
+    lowerChars = ""
+    upperChars = ""
+    numberChars = ""
+    specialChars = ""
+
+    storeAllCharsHere = "";
     // the other prompts
     lowerChars = confirm(
       "Would you like this in your password?\nLower case letters\nSelect 'OK' to include them. Click 'Cancel' to move on to the next option."
     );
     if (lowerChars === true) {
       storeAllCharsHere += everyChar.lowercase;
-      console.log(storeAllCharsHere)
+      console.log(storeAllCharsHere);
     }
 
     upperChars = confirm(
@@ -49,15 +53,14 @@ function generatePassword(randomString) {
     );
     if (upperChars === true) {
       storeAllCharsHere += everyChar.uppercase;
-      console.log(storeAllCharsHere)
+      console.log(storeAllCharsHere);
     }
     numberChars = confirm(
       "Would you like this in your password?\nNumbers\nSelect 'OK' to include them. Click 'Cancel' to move on to the next option."
     );
     if (numberChars === true) {
       storeAllCharsHere += everyChar.numbers;
-      console.log(storeAllCharsHere)
-
+      console.log(storeAllCharsHere);
     }
 
     specialChars = confirm(
@@ -65,28 +68,37 @@ function generatePassword(randomString) {
     );
     if (specialChars === true) {
       storeAllCharsHere += everyChar.special;
-      console.log(storeAllCharsHere)
+      console.log(storeAllCharsHere);
     }
-    console.log("These are the characters from which the generator will randomly select: " + storeAllCharsHere)
+    console.log(
+      "These are the characters from which the generator will randomly select: " +
+        storeAllCharsHere
+        
+        );
+        console.log("Lowercase: " + lowerChars)
+        console.log("Uppercase: " + upperChars)
+        console.log("Numbers: " + numberChars)
+        console.log("Special: " + specialChars)
+   
+      }
+      
+      if ((lowerChars === false) && (upperChars === false) && (numberChars === false) && (specialChars === false)) {
+    console.log('-------------------------')
+    console.log("ALL OF THEM ARE FALSE.")
+    runTheOtherPrompts = false
+
   }
-//⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️
-  // # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-  // # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-  // # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
   function passwordLength(askPasswordLength) {
     // Variable to store the randomly selected characters
     password = "";
-    randomIndex = storeAllCharsHere.length
+    randomIndex = storeAllCharsHere.length;
     // Variable to holds characters from which to randomly choose, and then store in the password variable.
-  
 
-    //⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️
     // Generate a random number. askPasswordLength parameter determines the range.
     function ranNumbers(askPasswordLength) {
       randomNumber = Math.floor(Math.random() * askPasswordLength);
       return randomNumber;
     }
-    //⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️
 
     for (let step = 0; step < askPasswordLength; step++) {
       password += storeAllCharsHere.charAt(ranNumbers(randomIndex));
@@ -94,15 +106,17 @@ function generatePassword(randomString) {
 
     return password;
   }
-  // # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-  // # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-  // # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
   if (askPasswordLength === false) {
     return "Your password doesn't meet the length requirement.\nPlease choose a number that is at least 8 and no larger than 128.\nClick 'Generate Password' to try again.";
-  } else {
+    
+  } 
+  else if (runTheOtherPrompts === false) {
+    return "You chose a length for your password, but you selected 'Cancel' for every prompt after that.\nPlease click 'OK' to at least one of the prompts so that the password generator knows the character types you want in your password."
+  }
+  else {
     randomString = passwordLength(askPasswordLength);
     thisMessage = "Here is your password: \n";
     return thisMessage + randomString;
   }
+
 }
